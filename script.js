a549bd1c6dd596328f49456e0734e7782e5c92ff
Parv Aggarwal <143114920+Parvaggarwal01@users.noreply.github.com>
2023-10-26 08:18:54 +0530

    major changes

--- script.js
+++ script.js
@@ -23,3 +23,81 @@ gsap.to("#main",{
         scrub: 2, 
     }
 })
+
+gsap.from("#page2 h1, #page2 img, #page2 p",{
+    y: 90,
+    opacity: 0,
+    duration: 2,
+    scrollTrigger:{
+        trigger:"#page2",
+        scroller:"body",
+        start:"top 70%",
+        end:"top 65%",
+        scrub:4,
+    }
+})
+
+gsap.from("#page3 h1, #page3 img, #page3 p",{
+    y: 90,
+    opacity: 0,
+    duration: 2,
+    scrollTrigger:{
+        trigger:"#page3",
+        scroller:"body",
+       // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:4,
+    }
+})
+
+gsap.from("#page4 h1, #page4 img, #page4 p",{
+    y: 90,
+    opacity: 0,
+    duration: 2,
+    scrollTrigger:{
+        trigger:"#page4",
+        scroller:"body",
+       // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:4,
+    }
+})
+
+var typed = new Typed('#element', {
+    strings: ["<i></i> What's Your Carbon Footprints? "],
+    typeSpeed: 50,
+    loop: true,
+    loopCount: Infinity,
+    backDelay: 1500,
+    backSpeed: 50,
+  });
+
+  gsap.from("#page5 #element, #page5 img,#page5 p, #page5 btn",{
+    y: 90,
+    opacity: 0,
+    duration: 2,
+    scrollTrigger:{
+        trigger:"#page5",
+        scroller:"body",
+       // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:4,
+    }
+})
+
+gsap.from("#page6 img,#page6 p",{
+    y: 90,
+    opacity: 0,
+    duration: 2,
+    scrollTrigger:{
+        trigger:"#page6",
+        scroller:"body",
+       // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:4,
+    }
+})
